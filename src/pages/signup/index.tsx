cc1b59aad503d1413fb8c13f1dc389f5e7b59023
김효진 <khj@gimhyojin-ui-MacBookPro.local>
2023-02-18 11:38:20 +0900

    beckend와 api test 완료

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,41 +1,66 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface ISubmitSignupForm {
+  username: string;
+  password: string;
+}
+
+interface IUser {
+  username: string;
+}
 
 const Signup = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [isSignup, setIsSignup] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ISubmitSignupForm>();
+
+  const onSubmitSignup: SubmitHandler<ISubmitSignupForm> = async (data) => {
+    const res = await axios.post('http://localhost:8001/users', data);
+    console.log('signup res', res);
+    if (res.status === 201) {
+      setIsSignup(true);
+      fetchUsers();
+    }
+  };
+
+  const fetchUsers = async () => {
+    const res = await axios.get('http://localhost:8001/users');
+    setUsers(res.data);
+  };
   useEffect(() => {
-    const res = axios.get('http://localhost:8001/users');
-    console.log('res', res);
+    fetchUsers();
   }, []);
 
   return (
     <div className='form-container'>
       <form
+        onSubmit={handleSubmit(onSubmitSignup)}
         style={{ display: 'flex', flexDirection: 'column', maxWidth: '300px' }}
       >
-        <label htmlFor='name'>Name</label>
+        <label htmlFor='username'>Email</label>
         <input
-          type='text'
-          id='name'
-          name='name'
-          required
-          style={{ padding: '3px', marginBottom: '5px' }}
-        />
-
-        <label htmlFor='email'>Email</label>
-        <input
-          type='email'
-          id='email'
-          name='email'
-          required
+          {...register('username')}
+          type='username'
+          id='username'
+          name='username'
           style={{ padding: '3px', marginBottom: '5px' }}
         />
 
         <label htmlFor='password'>Password</label>
         <input
+          {...register('password')}
           type='password'
           id='password'
           name='password'
-          required
           style={{ padding: '3px', marginBottom: '5px' }}
         />
 
@@ -69,6 +94,12 @@ const Signup = () => {
           </button>
         </div>
       </form>
+
+      <div style={{ margin: '10px' }}>
+        {users?.map((user) => {
+          return <div key={user.username}>{user.username}</div>;
+        })}
+      </div>
     </div>
   );
 };
